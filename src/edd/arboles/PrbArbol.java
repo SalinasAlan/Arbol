33092f36b7ee6ad01edd5dc8369691044daa490c
SalinasAlan <alansal_11@hotmail.com>
2017-11-04 16:02:17 -0600

    Se creó el prb para el árbol

---
+++ src/edd/arboles/PrbArbol.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.arboles;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Salinas
+ */
+public class PrbArbol
+{
+
+    BufferedReader mLeer = new BufferedReader(new InputStreamReader(System.in));    
+    Arbol mAr = new Arbol();
+}

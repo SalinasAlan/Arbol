e0a4f1c39f389ee882920d1a085dee0f80bb71c2
SalinasAlan <alansal_11@hotmail.com>
2017-11-04 16:03:44 -0600

    Se empezó a hacer las operaciones del árbol

--- src/edd/arboles/Arbol.java
+++ src/edd/arboles/Arbol.java
@@ -5,11 +5,44 @@
  */
 package edd.arboles;
 
+import edd.listasdobles.AlumnoArbol;
+
 /**
  *
  * @author Salinas
  */
 public class Arbol
 {
-    
+
+    private NodoArbol r;
+
+    public Arbol()
+    {
+        this.r = null;
+    }
+
+    /**
+     * @return the r
+     */
+    public NodoArbol getR()
+    {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(NodoArbol r)
+    {
+        this.r = r;
+    }
+
+    public void insertNodo(NodoArbol mNodo)
+    {
+        if (mNodo == null)
+        {
+
+        }
+    }
+
 }

bde4d6ad1de229ecc19bff724ddc225e7136ee64
SalinasAlan <alansal_11@hotmail.com>
2017-11-05 20:27:53 -0600

    Se agregó parte del menú

--- src/edd/arboles/PrbArbol.java
+++ src/edd/arboles/PrbArbol.java
@@ -15,6 +15,23 @@ import java.io.InputStreamReader;
 public class PrbArbol
 {
 
-    BufferedReader mLeer = new BufferedReader(new InputStreamReader(System.in));    
-    Arbol mAr = new Arbol();
+    public static void main(String[] args)
+    {
+        BufferedReader mLeer = new BufferedReader(new InputStreamReader(System.in));
+        Arbol mAr = new Arbol();
+        int op = 0;
+
+        do
+        {
+            System.out.println("\n1. Insertar grupo"
+                    + "\n2. Eliminar grupo"
+                    + "\n3. Modificar grupo");
+            switch (op)
+            {
+                case 1:
+                case 2:
+                case 3:
+            }
+        } while (op != 4);
+    }
 }
